7df15baeb43f83ee2fcd4a4bff67f5e7e2b3381a
Milan Lund <hello@milanlund.com>
2021-01-08 13:38:33 +0100

    KKD-423 - Remove reCaptcha v3 related code

--- helpers/recaptcha.js
+++ helpers/recaptcha.js
@@ -1,27 +1,6 @@
 const axios = require('axios');
 
 const recaptcha = {
-    checkv3: async (data) => {
-        if (process.env['Recaptcha-v3.secret']) {
-            const settings = {
-                secret: process.env['Recaptcha-v3.secret'],
-                response: data['g-recaptcha-response']
-            };
-
-            const response = await axios({
-                method: 'post',
-                url: 'https://www.google.com/recaptcha/api/siteverify?secret=' + settings.secret + '&response=' + settings.response
-            });
-
-            if (response.data.success === true && response.data.score >= 0.5) {
-                return true;
-            }
-
-            return false;
-        } else {
-            return true;
-        }
-    },
     checkv2: async (data) => {
         if (process.env['Recaptcha-v2.secret']) {
             const settings = {
