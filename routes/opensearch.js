46558393c0f77f634ac82bd267c9ebc53d977418
Milan Lund <hello@milanlund.com>
2021-02-22 13:34:30 +0100

    KKD-472 - Opensearch enhancements

--- routes/opensearch.js
+++ routes/opensearch.js
@@ -1,23 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const asyncHandler = require('express-async-handler');
 
 const helper = require('../helpers/helperFunctions');
-const handleCache = require('../helpers/handleCache');
-const commonContent = require('../helpers/commonContent');
 
-router.get('/', asyncHandler(async (req, res, next) => {
-  const home = await handleCache.ensureSingle(res, 'home', async () => {
-      return commonContent.getHome(res);
-  });
-
-  res.set('Content-Type', 'application/xml');
+router.get('/', (req, res) => {
+  res.set('Content-Type', 'application/opensearchdescription+xml');
 
   return res.render('tutorials/pages/opensearch', {
     req: req,
     domain: helper.getDomain(),
-    shortname: home && home.length ? home[0].title.value : 'Kentico Kontent Docs',
+    shortname: 'Kentico Kontent Docs'
   });
-}));
+});
 
 module.exports = router;
