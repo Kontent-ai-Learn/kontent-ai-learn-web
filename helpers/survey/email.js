aa319f99e23a0176e008c17cc09840e1ed0dbf21
Milan Lund <hello@milanlund.com>
2022-08-30 13:18:06 +0200

    KKD-754 - Filter empty surveys before sending notification to SendGrid/Teams

--- helpers/survey/email.js
+++ helpers/survey/email.js
@@ -22,9 +22,22 @@ const createNotification = (attempt) => {
   return notification;
 };
 
+const attemptHasAnswer = (attempt) => {
+  let hasAnswer = false;
+  attempt.questions.forEach((question) => {
+    if (question.answers.length) {
+      const selectedAnswer = question.answers.find((item) => item.selected);
+      if (selectedAnswer) hasAnswer = true;
+    } else if (question.text_answer) {
+      hasAnswer = true;
+    }
+  });
+  return hasAnswer;
+};
+
 const sendNotification = async (attempt) => {
   const recipient = process.env.SENDGRID_EMAIL_ADDRESS_SURVEY_NOTIFICATION_TO;
-  if (!recipient || !attempt) return;
+  if (!recipient || !attempt || !attemptHasAnswer(attempt)) return;
   const notification = createNotification(attempt);
   await sendGridEmail.send({
     recipient: recipient,
